f1fb572abf19a2f4b137b709cb579a8667ea3bfb
Clement Callejon <clement.callejon@tutanota.com>
2026-08-12 15:31:55 +0200

    feat: add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import AppHeader from './components/AppHeader.vue'
-import AppFooter from './components/AppFooter.vue'
-import MovieCard from './components/MovieCard.vue'
-import MovieDetails from './components/MovieDetails.vue'
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+import MovieCard from "./components/MovieCard.vue";
+import MovieDetails from "./components/MovieDetails.vue";
 </script>
 
 <template>
   <div id="app">
     <AppHeader />
-    <main>
+    <main class="mx-auto w-full max-w-7xl flex-1 px-4 py-10 sm:px-6 lg:px-8">
       <MovieCard />
       <MovieDetails />
     </main>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-  <footer>
-    <p>&copy; 2026 Movie Night</p>
+  <footer class="border-t border-slate-800 bg-slate-950">
+    <div class="mx-auto max-w-7xl px-4 py-6 text-center text-sm text-slate-500">
+      Movie Night - Trouvez votre prochain film.
+    </div>
   </footer>
 </template>
