0e97ad515598493417eddc739cc3cc0bca7cea9c
Clement Callejon <clement.callejon@tutanota.com>
2026-08-12 15:11:32 +0200

    chore: add imports to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
-
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import MovieCard from './components/MovieCard.vue'
+import MovieDetails from './components/MovieDetails.vue'
 </script>
 
 <template>
